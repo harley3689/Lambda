341d81659be55f716d6fd6c081e8919797658ff9
andrey <harley3689@gmail.com>
2022-09-22 11:19:14 +0300

    file add

--- src/Netology/part2/TreeMap/Main.java
+++ src/Netology/part2/TreeMap/Main.java
@@ -2,19 +2,18 @@ package Netology.part2.TreeMap;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         List<Person> people = new ArrayList<>();
 
-        people.add(new Person("Vasily","Dolgorukov-Krymsky",59));
-        people.add(new Person("Pyotr","Semyonov-Tyan-Shansky",67));
-        people.add(new Person("Arthur","Conan-Doyle",71));
-        people.add(new Person("Friedrich","Schiller",45));
-        people.add(new Person("François","Rabelais",55));
-        Collections.sort(people,new PersonsComparator(2));
+        people.add(new Person("Vasily", "Dolgorukov-Krymsky", 59));
+        people.add(new Person("Pyotr", "Semyonov-Tyan-Shansky", 67));
+        people.add(new Person("Arthur", "Conan-Doyle", 71));
+        people.add(new Person("Friedrich", "Schiller", 45));
+        people.add(new Person("François", "Rabelais", 55));
+        Collections.sort(people, new PersonsComparator(2));
         Collections.reverse(people);
         System.out.println(people);
     }
