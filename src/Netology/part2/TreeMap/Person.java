de231dba6a3ad48b005ba672586055cf3c76e5d9
andrey <harley3689@gmail.com>
2022-09-22 11:10:49 +0300

    file add

---
+++ src/Netology/part2/TreeMap/Person.java
@@ -0,0 +1,49 @@
+package Netology.part2.TreeMap;
+
+public class Person {
+    private String name;
+    private String surname;
+    private String[] surnameSize;
+    private int age;
+
+    public Person(String name, String surname, int age) {
+        this.name = name;
+        this.surname = surname;
+        this.surnameSize = surname.split("\\P{IsAlphabetic}+");
+        this.age = age;
+    }
+
+    public int size() {
+        int size = surnameSize.length;
+        return size;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return (name + " " + surname + " " + ":" + age);
+    }
+}
---
+++ src/Netology/part2/TreeMap/PersonsComparator.java
@@ -0,0 +1,44 @@
+package Netology.part2.TreeMap;
+
+import java.util.Comparator;
+
+public class PersonsComparator implements Comparator<Person> {
+    protected int words;
+
+    public PersonsComparator(int words) {
+        this.words = words;
+    }
+
+    /*@Override
+    public int compare(Person o1, Person o2) {
+        int surname1 = o1.getSurname().split("\\P{IsAlphabetic}+").length;
+        int surname2 = o2.getSurname().split("\\P{IsAlphabetic}+").length;
+
+        if (surname1 > surname2 || surname1 < surname2) {
+            return Integer.compare(surname1, surname2);
+        } else {
+            return Integer.compare(o1.getAge(), o2.getAge());
+        }
+    }*/
+    /*@Override
+    public int compare(Person o1, Person o2) {
+        String[] surname1 = o1.getSurname().split("\\P{IsAlphabetic}+");
+        String[] surname2 = o2.getSurname().split("\\P{IsAlphabetic}+");
+
+            if ((surname1.length >= words) && (words <= surname2.length)) {
+                return Integer.compare(surname1.length, surname2.length);
+            } else {
+                return Integer.compare(o1.getAge(), o2.getAge());
+            }
+    }*/
+    @Override
+    public int compare(Person o1, Person o2) {
+        if (o1.size() > words || o2.size() > words) {
+            return Integer.compare(o1.size(), o2.size());
+        } else if (o1.size() == o2.size()) {
+            return Integer.compare(o1.getAge(), o2.getAge());
+        }
+        return Integer.compare(o1.getAge(), o2.getAge());
+    }
+
+}
