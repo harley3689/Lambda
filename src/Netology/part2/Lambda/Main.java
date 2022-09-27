4a2d6a783c154b0cd9bd843d76f42963a3ef86ab
andrey <harley3689@gmail.com>
2022-09-27 19:46:16 +0300

    "first"

---
+++ src/Netology/part2/Lambda/Main.java
@@ -0,0 +1,23 @@
+package Netology.part2.Lambda;
+
+import javax.sql.rowset.Predicate;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+
+public class Main {
+    public static void main(String[] args) {
+        List<Person> people = new ArrayList<>();
+
+        people.add(new Person("Vasily", "Dolgorukov Krymsky", 5));
+        people.add(new Person("Pyotr", "Semyonov Tyan Shansky", 60));
+        people.add(new Person("Arthur", "Conan Doyle", 17));
+        people.add(new Person("Friedrich", "Schiller", 18));
+        people.add(new Person("François", "Rabelais", 65));
+
+        people.removeIf(person -> person.getAge() < 18);
+        people.forEach(System.out::println);
+    }
+}
+
